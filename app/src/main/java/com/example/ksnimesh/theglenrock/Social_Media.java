ca83380c007f6aea8d31838cd3713106e4bbf4e7
KSNimesh <shadeeka99@gmail.com>
2018-11-19 20:37:02 +0530

    Updates notification

---
+++ app/src/main/java/com/example/ksnimesh/theglenrock/Social_Media.java
@@ -0,0 +1,17 @@
+package com.example.ksnimesh.theglenrock;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+
+public class Social_Media extends AppCompatActivity {
+  Button fb,insta,tripadvisor;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate( savedInstanceState );
+        setContentView( R.layout.activity_social__media );
+
+
+
+    }
+}
